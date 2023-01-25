6ce0e9592e366c91742b69c9834905d7865cc1ae
TheJessDev <121843183+TheJessDev@users.noreply.github.com>
2023-01-25 02:01:00 -0700

    301-first-assignment

---
+++ scripts/a1 script.js
@@ -0,0 +1,72 @@
+console.log("Scripts");
+
+var product1="Smart TV";
+var product2="Robot Vaccumm";
+var product3="Gaming Desk";
+var product4="Touchscreen Laptop";
+var product5="TV Mount";
+
+var product6="Scented Candle";
+var product7="Coffee Maker";
+var product8="Floor Lamp";
+var product9="Wall Clock";
+var product10="Artificial Plant";
+
+var product1Price=298.00;
+var product2Price=96.00;
+var product3Price=79.00;
+var product4Price=159.00;
+var product5Price=39.99;
+
+var product6Price=13.51;
+var product7Price=168.99;
+var product8Price=14.88;
+var product9Price=34.64;
+var product10Price=67.99;
+
+
+
+
+document.write('<p class="text"> This '+ product1+' is priced at $'+ product1Price+'.00.</p>')
+
+document.write('<p class="text"> This '+ product2+' is priced at $'+ product2Price+'.00.</p>')
+
+document.write('<p class="text"> This '+ product3+' is priced at $'+ product3Price+'.00.</p>')
+
+document.write('<p class="text"> This '+ product4+' is priced at $'+ product4Price+'.00.</p>')
+
+document.write('<p class="text"> This '+ product5+' is priced at $'+ product5Price+'.</p>')
+
+document.write('<p class="text"> This '+ product6+' is priced at $'+ product6Price+'.</p>')
+
+document.write('<p class="text"> This '+ product7+' is priced at $'+ product7Price+'.</p>')
+
+document.write('<p class="text"> This '+ product8+' is priced at $'+ product8Price+'.</p>')
+
+document.write('<p class="text"> This '+ product9+' is priced at $'+ product9Price+'.</p>')
+
+document.write('<p class="text"> This '+ product10+' is priced at $'+ product10Price+'.</p>')
+
+
+
+document.write(`<p class="text"> Buy a ${product1} and a ${product5} for a total of $${product1Price + product5Price} .</p>`)
+
+document.write(`<p class="text"> Buy a ${product3} and a ${product4} for a total of $${product3Price + product4Price} .00.</p>`)
+
+document.write(`<p class="text"> Buy a ${product2} and a ${product3} for a total of $${product2Price + product3Price} .00.</p>`)
+
+document.write(`<p class="text"> Buy a ${product1} and a ${product4} for a total of $${product1Price + product4Price} .00.</p>`)
+
+document.write(`<p class="text"> Buy a ${product3} and a ${product1} for a total of $${product3Price + product1Price} .00.</p>`)
+
+document.write(`<p class="text"> Buy a ${product6} and a ${product7} for a total of $${product6Price + product7Price} .</p>`)
+
+document.write(`<p class="text"> Buy a ${product7} and a ${product8} for a total of $${product7Price + product8Price} .</p>`)
+
+document.write(`<p class="text"> Buy a ${product9} and a ${product10} for a total of $${product9Price + product10Price} .</p>`)
+
+document.write(`<p class="text"> Buy a ${product6} and a ${product10} for a total of $${product6Price + product10Price} .</p>`)
+
+document.write(`<p class="text"> Buy a ${product7} and a ${product9} for a total of $${produc71Price + product9Price} .</p>`)
+
+document.write(`<p class="text"> Buy a ${product8} and a ${product9} for a total of $${product8Price + product9Price} .</p>`)
